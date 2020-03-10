7dac61e16ea3e80bb532c9550c2353ae5c003cf4
tony <tirlipinpon@hotmail.com>
2020-03-10 14:44:20 +0100

    paralax

--- server/public/desktop/js/game.js
+++ server/public/desktop/js/game.js
@@ -44,6 +44,9 @@ window.onbeforeunload = function (e) {
 function preload() {
     var self = this;
     self.load.image('ship', '/public/desktop/assets/spaceShips_001.png');
+    this.load.image('background', '/public/desktop/assets/background.png');
+    this.load.image('parallax_1', '/public/desktop/assets/parallax1.png');
+    this.load.image('parallax_2', '/public/desktop/assets/parallax2.png');
 
     socket.emit('desktop connected');
     self.numberPlayers = getText(self);
@@ -51,10 +54,19 @@ function preload() {
 
 function create() {
     self = this;
+
+    this.background = setBackground(self);
+    console.log(this.background.depth);
+    this.parallax_1 = setParallax(0, 0, 50, 'parallax_1');
+    this.parallax_2 = setParallax(self.game.config.width - 50, 0, 50, 'parallax_2');
+
+
     playersGroup = this.add.group();
     socket.on('desktop nbPlayers', function (nbPlayers) {
         // console.log('nbPlayers : ' + nbPlayers);
-        self.numberPlayers.setText(nbPlayers + ' player')
+        self.numberPlayers.setText(nbPlayers + ' player');
+        self.numberPlayers.setDepth(1);
+        console.log(self.numberPlayers.depth);
     });
     socket.on('desktop new player', function (playerInfo) {
         console.log('C - new player : ', playerInfo);
@@ -84,12 +96,36 @@ function create() {
     });
 }
 
+
+
 function update() {
     var self = this;
     // if (text) {
     //     console.log(elapsed.getProgress().toString().substr(0, 4))
     //     text.setText('Event.progress: ' + elapsed.getProgress().toString().substr(0, 4));
     // }
+    if (gameStarted) {
+        this.background.tilePositionY -= 0.5;
+        this.parallax_1.tilePositionY -= 1.5;
+        this.parallax_2.tilePositionY -= 1.5;
+    }
+}
+
+function setParallax(x, y, size, name) {
+    this.parallax = self.add.tileSprite(x, y, size, self.game.config.height, name);
+    this.parallax.setOrigin(0, 0);
+    this.parallax.alpha = 0.5;
+    // this.parallax_1.scaleX = 1.2;
+    // this.parallax_1.setPosition(-35, 0);
+    // this.parallax_1.setScrollFactor(0);
+    return this.parallax;
+}
+
+function setBackground(self) {
+    var background = self.add.tileSprite(0, 0, self.game.config.width, self.game.config.height, 'background');
+    background.setOrigin(0, 0);
+    background.setDisplaySize(self.game.config.width, self.game.config.height);
+    return background;
 }
 
 function formatTime(seconds){
@@ -181,7 +217,7 @@ function onEventCountdown() {
 
 function onEventCountdownGameEnd() {
     timeGame -= 1;
-    if (initialTime < 0) {
+    if (timeGame < 0) {
         timedEvent.destroy();
         text.destroy();
         gameStarted = false;
@@ -190,15 +226,15 @@ function onEventCountdownGameEnd() {
         if (timeGame < 30 && timeGame > 20) {
             text.displayWidth += 2;
             text.displayHeight += 2;
-            text.setAlpha(.2);
+            text.setAlpha(.1);
         } else if (timeGame < 20 && timeGame > 10) {
             text.displayWidth += 10;
             text.displayHeight += 10;
-            text.setAlpha(.3);
+            text.setAlpha(.2);
         } else if (timeGame < 10) {
             text.displayWidth += 20;
             text.displayHeight += 20;
-            text.setAlpha(.4);
+            text.setAlpha(.3);
         }
         text.setText(formatTime(timeGame));
     }
