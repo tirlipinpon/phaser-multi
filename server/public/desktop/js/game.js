ccd51984dd66ab6f10933b44bc8cf00309bde8f5
tony <tirlipinpon@hotmail.com>
2020-03-17 14:18:15 +0100

    server player number + desktop order funtion

--- server/public/desktop/js/game.js
+++ server/public/desktop/js/game.js
@@ -62,6 +62,28 @@ function preload() {
     self.load.spritesheet('power-up', '/public/desktop/assets/spritesheets/power-up.png', { frameWidth: 64, frameHeight: 64 });
     self.numberPlayers = getText(self);
 }
+function update() {
+    var self = this;
+    // if (text) {
+    //     console.log(elapsed.getProgress().toString().substr(0, 4))
+    //     text.setText('Event.progress: ' + elapsed.getProgress().toString().substr(0, 4));
+    // }
+
+    if (gameStarted) {
+        this.background.tilePositionY -= 0.5;
+        this.parallax_1.tilePositionY -= 1.5;
+        this.parallax_2.tilePositionY -= 1.5;
+        enemies.getChildren().forEach(function (ship) {
+            moveShip(ship, globalSpeed + ship.speed);
+        });
+        powerUps.getChildren().forEach(function (powerUp) {
+            // console.log(' limit:' + (self.game.config.height - 20) + ' actu: ' + powerUp.y);
+            if (powerUp.y > (self.game.config.height - 15)) {
+                powerUp.destroy();
+            }
+        });
+    }
+}
 function create() {
     self = this;
     self.background = setBackground(self);
@@ -77,6 +99,8 @@ function create() {
     timeEventPowerUp = self.time.addEvent({delay: 7000, callback: createPowerUp, callbackScope: this, loop: true});
 
 }
+
+// game general
 function socketOn() {
     socket.on('desktop nbPlayers', function (nbPlayers) {
         self.numberPlayers.setText(nbPlayers + ' player');
@@ -138,122 +162,8 @@ function socketOn() {
 
     });
 }
-function update() {
-    var self = this;
-    // if (text) {
-    //     console.log(elapsed.getProgress().toString().substr(0, 4))
-    //     text.setText('Event.progress: ' + elapsed.getProgress().toString().substr(0, 4));
-    // }
-
-    if (gameStarted) {
-        this.background.tilePositionY -= 0.5;
-        this.parallax_1.tilePositionY -= 1.5;
-        this.parallax_2.tilePositionY -= 1.5;
-        enemies.getChildren().forEach(function (ship) {
-            moveShip(ship, globalSpeed + ship.speed);
-        });
-        powerUps.getChildren().forEach(function (powerUp) {
-            // console.log(' limit:' + (self.game.config.height - 20) + ' actu: ' + powerUp.y);
-            if (powerUp.y > (self.game.config.height - 15)) {
-                powerUp.destroy();
-            }
-        });
-    }
-}
-function createPowerUp() {
-    if (powerUps.getChildren().length < 1) {
-        var powerUp = self.physics.add.sprite(64, 64, 'power-up');
-        powerUps.add(powerUp);
-        powerUp.setRandomPosition(0, 0, self.game.config.width, 50);
-
-        if (Math.random() > 0.5) {
-            powerUp.play('red_anim');
-        } else {
-            powerUp.play('gray_anim');
-        }
-        powerUp.setVelocity(100, 100);
-        powerUp.setCollideWorldBounds(true);
-        powerUp.setBounceX(1);
-        powerUp.setBounceY(1);
-    }
-}
-function movePlayerManager(player, action) {
-    if (action === 'swipeleft') {
-        player.setVelocityX(-200);
-    } else if (action === 'swiperight') {
-        player.setVelocityX(200);
-    }
-
-    if (action === 'swipeup') {
-        player.setVelocityY(-100);
-    } else if (action === 'swipedown') {
-        player.setVelocityY(100);
-    }
-}
-function setNewPositionPlayers(playerInfo) {
-    if (playerInfo) {
-        playersGroup.getChildren().forEach(function (player) {
-            if (playerInfo.id === player.id) {
-                player.x = positionPlayerExtra[playerInfo.position];
-                player.position = playerInfo.position;
-                removeHealthBar(player.id);
-                removeScore(player.id);
-                drawHealthBar(self, player.health, player.x, player.color, player.id, player.position);
-                drawScores(self, player.x, player.id, player.position);
-            }
-        });
-    }
-}
 function setOverlap() {
     self.physics.add.overlap(playersGroup, enemies, this.hurtPlayer, null, this);
-}
-function hurtPlayer(player, enemy) {
-    console.log('hurtPlayer');
-    this.resetShipPos(enemy);
-    if (player.alpha < 1) {
-        return
-    }
-    new Explosion(self, player.x, player.y);
-    player.disableBody(true, true);
-    player.health -= enemy.speed * 10;
-    if (player.health < 0) {
-        player.health = 0;
-    }
-    drawHealthBar(self,
-        player.health,
-        healthBars[player.id].x,
-        player.color,
-        player.id,
-        player.position);
-    self.time.addEvent({
-        delay: 1000,
-        callback: this.resetPlayer(player),
-        callbackScope: this,
-        loop: false
-    });
-    if (player.health < 1) {
-        removePlayerFromPhaser(self, player.id);
-    }
-}
-function resetPlayer(player) {
-    var x = positionPlayerExtra[player.position];
-    var y = self.game.config.height + 164;
-    player.enableBody(true, x, y, true, true);
-    player.alpha = 0.5;
-    var tween = self.tweens.add({
-        targets: player,
-        y: self.game.config.height - 64,
-        ease: 'Power1',
-        repeat: 0,
-        onComplete: function () {
-            player.alpha = 1
-        },
-        callbackScope: this
-    });
-}
-function resetShipPos(ship) {
-    ship.y = 0;
-    ship.x = getRandomX(50, this.game.config.width-50);
 }
 function createAnims() {
     self.anims.create({
@@ -324,53 +234,8 @@ function createAnims() {
         repeat: -1 // infinity loop -1
     });
 }
-function duplicateEnnemies() {
-    for (var i = 1; i < 4; i++) {
-        var ship = self.add.sprite(getRandomX(0, self.game.config.width), 0, 'ship' + i);
-        ship.speed = i;
-        ship.play('ship' + i + '_anim');
-        enemies.add(ship);
-    }
-}
 function getRandomX(min, max) {
     return Phaser.Math.Between(min, max);
-}
-function duplicateEnnemiesByTime() {
-    var temp = timeGame % 20;
-    if (temp === 0) {
-        duplicateEnnemies()
-    }
-}
-function moveShip(ship, speed) {
-    ship.y += speed;
-    if (ship.y > this.game.config.height) {
-        resetShipPos(ship);
-    }
-}
-function setParallax(x, y, size, name) {
-    this.parallax = self.add.tileSprite(x, y, size, self.game.config.height, name);
-    this.parallax.setOrigin(0, 0);
-    this.parallax.alpha = 0.5;
-    // this.parallax_1.scaleX = 1.2;
-    // this.parallax_1.setPosition(-35, 0);
-    // this.parallax_1.setScrollFactor(0);
-    return this.parallax;
-}
-function setBackground(self) {
-    var background = self.add.tileSprite(0, 0, self.game.config.width, self.game.config.height, 'background');
-    background.setOrigin(0, 0);
-    background.setDisplaySize(self.game.config.width, self.game.config.height);
-    return background;
-}
-function formatTime(seconds) {
-    // Minutes
-    // var minutes = Math.floor(seconds/60);
-    // Seconds
-    var partInSeconds = seconds;
-    // Adds left zeros to seconds
-    partInSeconds = partInSeconds.toString().padStart(2, '0');
-    // Returns formated time
-    return partInSeconds;
 }
 function setTimerStartGame(self) {
     // var timer = self.scene.time.addEvent({
@@ -387,86 +252,6 @@ function setTimerStartGame(self) {
     // this.elapsed = timer.delayedCall(3000, onEvent, [], this);
     this.elapsed = self.time.delayedCall(3000, onEvent, [], this);
     text = self.add.text('Event.progress: ' + this.elapsed.getProgress().toString().substr(0, 4));
-}
-function getText(self) {
-    var style = {font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle"};
-    var text = self.add.text(window.innerWidth / 2 - 100, window.innerHeight / 2, '0 player', style);
-    text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
-    text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
-    text.setOrigin(.5);
-    text.setAlpha(.4);
-    return text;
-}
-function addPlayerToPhaser(self, playerInfo) {
-    const player = self.physics.add.sprite(positionPlayerExtra[playerInfo.position], playerInfo.y, 'player');
-    player.id = playerInfo.id;
-    player.color = playerInfo.color;
-    player.position = playerInfo.position;
-    player.health = playerInfo.health;
-    player.score = playerInfo.score;
-    player.projectilesGroup = self.add.group();
-    player.setCollideWorldBounds(true);
-    playersGroup.add(player);
-    timeGame += 30;
-    self.physics.add.overlap(player.projectilesGroup, enemies, hitEnemy, null, this);
-    self.physics.add.collider(player.projectilesGroup, powerUps, function (projectile, powerUp) {
-        projectile.destroy();
-    });
-    self.physics.add.overlap(player, powerUps, this.pickPowerUp, null, this);
-}
-function pickPowerUp(player, powerUp) {
-    // the twoo params set to true, make it inactive and hide it from the display list.
-    powerUp.disableBody(true, true);
-    if (timerPowerUp) {
-        timerPowerUp.remove();
-    }
-    if (powerUp.anims.currentAnim.key === 'gray_anim') {
-        player.powerUp = 'gray_anim';
-        timerPowerUp = self.time.delayedCall(5000, function () {
-            player.powerUp = null;
-        }, [], this);
-    } else if (powerUp.anims.currentAnim.key === 'red_anim') {
-        player.powerUp = 'red_anim';
-        timerPowerUp = self.time.delayedCall(5000, function () {
-            player.powerUp = null;
-        }, [], this);
-    }
-}
-function hitEnemy(projectile, enemy) {
-    var explosion = new Explosion(self, enemy.x, enemy.y);
-    projectile.destroy();
-    this.resetShipPos(enemy);
-    playersGroup.getChildren().forEach(function (player) {
-        if (projectile.id === player.id) {
-            player.score += enemy.speed * 10;
-            scores[player.id].setText(this.zeroPad(player.score, 6));
-        }
-    });
-}
-function removePlayerFromPhaser(self, playerId) {
-    playersGroup.getChildren().forEach(function (player) {
-        if (playerId === player.id) {
-            player.destroy();
-            if (!gameStarted) {
-                timeGame -= 30;
-            }
-        }
-    });
-    if (playersGroup.getChildren().length === 0) {
-        gameStarted = false;
-    }
-}
-function removeHealthBar(playerId) {
-    if (healthBars[playerId]) {
-        healthBars[playerId].clear();
-        delete healthBars[playerId];
-    }
-}
-function removeScore(playerId) {
-    if (scores[playerId]) {
-        scores[playerId].destroy();
-        delete scores[playerId];
-    }
 }
 function onEventCountdown() {
     initialTime -= 1; // One second
@@ -517,6 +302,54 @@ function onEventCountdownGameEnd() {
         }
         duplicateEnnemiesByTime();
         text.setText(formatTime(timeGame));
+    }
+}
+
+// decors
+function setParallax(x, y, size, name) {
+    this.parallax = self.add.tileSprite(x, y, size, self.game.config.height, name);
+    this.parallax.setOrigin(0, 0);
+    this.parallax.alpha = 0.5;
+    // this.parallax_1.scaleX = 1.2;
+    // this.parallax_1.setPosition(-35, 0);
+    // this.parallax_1.setScrollFactor(0);
+    return this.parallax;
+}
+function setBackground(self) {
+    var background = self.add.tileSprite(0, 0, self.game.config.width, self.game.config.height, 'background');
+    background.setOrigin(0, 0);
+    background.setDisplaySize(self.game.config.width, self.game.config.height);
+    return background;
+}
+function formatTime(seconds) {
+    // Minutes
+    // var minutes = Math.floor(seconds/60);
+    // Seconds
+    var partInSeconds = seconds;
+    // Adds left zeros to seconds
+    partInSeconds = partInSeconds.toString().padStart(2, '0');
+    // Returns formated time
+    return partInSeconds;
+}
+function getText(self) {
+    var style = {font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle"};
+    var text = self.add.text(window.innerWidth / 2 - 100, window.innerHeight / 2, '0 player', style);
+    text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
+    text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
+    text.setOrigin(.5);
+    text.setAlpha(.4);
+    return text;
+}
+function removeHealthBar(playerId) {
+    if (healthBars[playerId]) {
+        healthBars[playerId].clear();
+        delete healthBars[playerId];
+    }
+}
+function removeScore(playerId) {
+    if (scores[playerId]) {
+        scores[playerId].destroy();
+        delete scores[playerId];
     }
 }
 function drawHealthBar(self, life, x, color, id, position) {
@@ -570,4 +403,181 @@ function zeroPad(number, size) {
         stringNumber = '0' + stringNumber;
     }
     return stringNumber;
+}
+
+//power up
+function createPowerUp() {
+    if (powerUps.getChildren().length < 1) {
+        var powerUp = self.physics.add.sprite(64, 64, 'power-up');
+        powerUps.add(powerUp);
+        powerUp.setRandomPosition(0, 0, self.game.config.width, 50);
+
+        if (Math.random() > 0.5) {
+            powerUp.play('red_anim');
+        } else {
+            powerUp.play('gray_anim');
+        }
+        powerUp.setVelocity(100, 100);
+        powerUp.setCollideWorldBounds(true);
+        powerUp.setBounceX(1);
+        powerUp.setBounceY(1);
+    }
+}
+function pickPowerUp(player, powerUp) {
+    // the twoo params set to true, make it inactive and hide it from the display list.
+    powerUp.disableBody(true, true);
+    if (timerPowerUp) {
+        timerPowerUp.remove();
+    }
+    if (powerUp.anims.currentAnim.key === 'gray_anim') {
+        player.powerUp = 'gray_anim';
+        timerPowerUp = self.time.delayedCall(5000, function () {
+            player.powerUp = null;
+        }, [], this);
+    } else if (powerUp.anims.currentAnim.key === 'red_anim') {
+        player.powerUp = 'red_anim';
+        timerPowerUp = self.time.delayedCall(5000, function () {
+            player.powerUp = null;
+        }, [], this);
+    }
+}
+
+// player
+function movePlayerManager(player, action) {
+    if (action === 'swipeleft') {
+        player.setVelocityX(-200);
+    } else if (action === 'swiperight') {
+        player.setVelocityX(200);
+    }
+
+    if (action === 'swipeup') {
+        player.setVelocityY(-100);
+    } else if (action === 'swipedown') {
+        player.setVelocityY(100);
+    }
+}
+function setNewPositionPlayers(playerInfo) {
+    if (playerInfo) {
+        playersGroup.getChildren().forEach(function (player) {
+            if (playerInfo.id === player.id) {
+                player.x = positionPlayerExtra[playerInfo.position];
+                player.position = playerInfo.position;
+                removeHealthBar(player.id);
+                removeScore(player.id);
+                drawHealthBar(self, player.health, player.x, player.color, player.id, player.position);
+                drawScores(self, player.x, player.id, player.position);
+            }
+        });
+    }
+}
+function hurtPlayer(player, enemy) {
+    console.log('hurtPlayer');
+    this.resetShipPos(enemy);
+    if (player.alpha < 1) {
+        return
+    }
+    new Explosion(self, player.x, player.y);
+    player.disableBody(true, true);
+    player.health -= enemy.speed * 10;
+    if (player.health < 0) {
+        player.health = 0;
+    }
+    drawHealthBar(self,
+        player.health,
+        healthBars[player.id].x,
+        player.color,
+        player.id,
+        player.position);
+    self.time.addEvent({
+        delay: 1000,
+        callback: this.resetPlayer(player),
+        callbackScope: this,
+        loop: false
+    });
+    if (player.health < 1) {
+        removePlayerFromPhaser(self, player.id);
+    }
+}
+function resetPlayer(player) {
+    var x = positionPlayerExtra[player.position];
+    var y = self.game.config.height + 164;
+    player.enableBody(true, x, y, true, true);
+    player.alpha = 0.5;
+    var tween = self.tweens.add({
+        targets: player,
+        y: self.game.config.height - 64,
+        ease: 'Power1',
+        repeat: 0,
+        onComplete: function () {
+            player.alpha = 1
+        },
+        callbackScope: this
+    });
+}
+function addPlayerToPhaser(self, playerInfo) {
+    const player = self.physics.add.sprite(positionPlayerExtra[playerInfo.position], playerInfo.y, 'player');
+    player.id = playerInfo.id;
+    player.color = playerInfo.color;
+    player.position = playerInfo.position;
+    player.health = playerInfo.health;
+    player.score = playerInfo.score;
+    player.projectilesGroup = self.add.group();
+    player.setCollideWorldBounds(true);
+    playersGroup.add(player);
+    timeGame += 30;
+    self.physics.add.overlap(player.projectilesGroup, enemies, hitEnemy, null, this);
+    self.physics.add.collider(player.projectilesGroup, powerUps, function (projectile, powerUp) {
+        projectile.destroy();
+    });
+    self.physics.add.overlap(player, powerUps, this.pickPowerUp, null, this);
+}
+function removePlayerFromPhaser(self, playerId) {
+    playersGroup.getChildren().forEach(function (player) {
+        if (playerId === player.id) {
+            player.destroy();
+            if (!gameStarted) {
+                timeGame -= 30;
+            }
+        }
+    });
+    if (playersGroup.getChildren().length === 0) {
+        gameStarted = false;
+    }
+}
+
+// enemies / ship
+function resetShipPos(ship) {
+    ship.y = 0;
+    ship.x = getRandomX(50, this.game.config.width-50);
+}
+function duplicateEnnemies() {
+    for (var i = 1; i < 4; i++) {
+        var ship = self.add.sprite(getRandomX(0, self.game.config.width), 0, 'ship' + i);
+        ship.speed = i;
+        ship.play('ship' + i + '_anim');
+        enemies.add(ship);
+    }
+}
+function duplicateEnnemiesByTime() {
+    var temp = timeGame % 20;
+    if (temp === 0) {
+        duplicateEnnemies()
+    }
+}
+function moveShip(ship, speed) {
+    ship.y += speed;
+    if (ship.y > this.game.config.height) {
+        resetShipPos(ship);
+    }
+}
+function hitEnemy(projectile, enemy) {
+    var explosion = new Explosion(self, enemy.x, enemy.y);
+    projectile.destroy();
+    this.resetShipPos(enemy);
+    playersGroup.getChildren().forEach(function (player) {
+        if (projectile.id === player.id) {
+            player.score += enemy.speed * 10;
+            scores[player.id].setText(this.zeroPad(player.score, 6));
+        }
+    });
 }
